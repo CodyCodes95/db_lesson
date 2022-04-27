d74e0ac3c96d573202fd9ee2597741eab86f3987
Cody Partington <codythatsme@gmail.com>
2022-04-27 11:28:35 +1000

    Changes made

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  # DEBUG - REMOVE FOR PRODUCTION!!!
+  skip_before_action :verify_authenticity_token
+
   def index
     @books = Book.all
   end
@@ -9,5 +12,22 @@ class BooksController < ApplicationController
     rescue
       render plain: "Book not found!"
     end
+  end
+
+  def create
+    book = Book.create(book_params)
+    redirect_to book
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book
+  end
+
+  private
+
+  def book_params
+    return params.permit(:title, :author_id)
   end
 end
